4ddee9707e1eb84c01538e0181abaebd3ece4418
cyyang <cyyang023@gmail.com>
2023-03-27 21:40:46 +0800

    Refactor LED blink demo

--- Core/led_task.h
+++
@@ -1,6 +0,0 @@
-#ifndef LED_TASK_H
-#define LED_TASK_H
-
-void vStartLEDFlashTasks(UBaseType_t uxPriority);
-
-#endif
--- Core/main.c
+++ Core/main.c
@@ -36,143 +36,108 @@
 #include "task.h"
 #include "timers.h"
 
-
-/* Demo application includes. */
-#include "led_task.h"
-
 /* Hardware and starter kit includes. */
 #include "NuMicro.h"
 #include "system.h"
 
-/* Priorities for the demo application tasks. */
-#define mainFLASH_TASK_PRIORITY   (tskIDLE_PRIORITY + 1UL)
-#define mainQUEUE_POLL_PRIORITY   (tskIDLE_PRIORITY + 2UL)
-#define mainSEM_TEST_PRIORITY     (tskIDLE_PRIORITY + 1UL)
-#define mainBLOCK_Q_PRIORITY      (tskIDLE_PRIORITY + 2UL)
-#define mainCREATOR_TASK_PRIORITY (tskIDLE_PRIORITY + 3UL)
-#define mainFLOP_TASK_PRIORITY    (tskIDLE_PRIORITY)
-#define mainCHECK_TASK_PRIORITY   (tskIDLE_PRIORITY + 3UL)
+#define MILLI_SECOND_TICK (configTICK_RATE_HZ / 1000)
 
-#define mainCHECK_TASK_STACK_SIZE (configMINIMAL_STACK_SIZE)
+void task1()
+{
+    while (1) {
+        PH4 ^= 1;
+        vTaskDelay(MILLI_SECOND_TICK * 3000);
+    }
+}
 
-/* The time between cycles of the 'check' task. */
-#define mainCHECK_DELAY ((portTickType) 5000 / portTICK_RATE_MS)
-
-/* The LED used by the check timer. */
-#define mainCHECK_LED (3UL)
-
-/* A block time of zero simply means "don't block". */
-#define mainDONT_BLOCK (0UL)
-
-/* The period after which the check timer will expire, in ms, provided no errors
-have been reported by any of the standard demo tasks.  ms are converted to the
-equivalent in ticks using the portTICK_RATE_MS constant. */
-#define mainCHECK_TIMER_PERIOD_MS (3000UL / portTICK_RATE_MS)
-
-/* The period at which the check timer will expire, in ms, if an error has been
-reported in one of the standard demo tasks.  ms are converted to the equivalent
-in ticks using the portTICK_RATE_MS constant. */
-#define mainERROR_CHECK_TIMER_PERIOD_MS (200UL / portTICK_RATE_MS)
-
-/* Set mainCREATE_SIMPLE_LED_FLASHER_DEMO_ONLY to 1 to create a simple demo.
-Set mainCREATE_SIMPLE_LED_FLASHER_DEMO_ONLY to 0 to create a much more
-comprehensive test application.  See the comments at the top of this file, and
-the documentation page on the http://www.FreeRTOS.org web site for more
-information. */
-#define mainCREATE_SIMPLE_LED_FLASHER_DEMO_ONLY 0
-
+void task2()
+{
+    while (1) {
+        PH5 ^= 1;
+        vTaskDelay(MILLI_SECOND_TICK * 1000);
+    }
+}
 /*-----------------------------------------------------------*/
 
 int main(void)
 {
     system_init();
 
-    /* Start standard demo/test application flash tasks.  See the comments at
-    the top of this file.  The LED flash tasks are always created.  The other
-    tasks are only created if mainCREATE_SIMPLE_LED_FLASHER_DEMO_ONLY is set to
-    0 (at the top of this file).  See the comments at the top of this file for
-    more information. */
-    vStartLEDFlashTasks(mainFLASH_TASK_PRIORITY);
-
-    /* The following function will only create more tasks and timers if
-    mainCREATE_SIMPLE_LED_FLASHER_DEMO_ONLY is set to 0 (at the top of this
-    file).  See the comments at the top of this file for more information. */
-    // prvOptionallyCreateComprehensveTestApplication();
+    xTaskCreate(task1, "task1", 256, NULL, tskIDLE_PRIORITY + 2, NULL);
+    xTaskCreate(task2, "task2", 256, NULL, tskIDLE_PRIORITY + 1, NULL);
 
     printf("FreeRTOS is starting ...\n");
 
     /* Start the scheduler. */
     vTaskStartScheduler();
 
-    /* If all is well, the scheduler will now be running, and the following line
-    will never be reached.  If the following line does execute, then there was
-    insufficient FreeRTOS heap memory available for the idle and/or timer tasks
-    to be created.  See the memory management section on the FreeRTOS web site
-    for more details. */
     for (;;)
         ;
 }
 /*-----------------------------------------------------------*/
 
-void vApplicationMallocFailedHook(void)
-{
-    /* vApplicationMallocFailedHook() will only be called if
-    configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.  It is a hook
-    function that will get called if a call to pvPortMalloc() fails.
-    pvPortMalloc() is called internally by the kernel whenever a task, queue,
-    timer or semaphore is created.  It is also called by various parts of the
-    demo application.  If heap_1.c or heap_2.c are used, then the size of the
-    heap available to pvPortMalloc() is defined by configTOTAL_HEAP_SIZE in
-    FreeRTOSConfig.h, and the xPortGetFreeHeapSize() API function can be used
-    to query the size of free heap space that remains (although it does not
-    provide information on how the remaining heap might be fragmented). */
-    taskDISABLE_INTERRUPTS();
-    for (;;)
-        ;
-}
+// void vApplicationMallocFailedHook(void)
+// {
+//     /* vApplicationMallocFailedHook() will only be called if
+//     configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.  It is a
+//     hook function that will get called if a call to pvPortMalloc() fails.
+//     pvPortMalloc() is called internally by the kernel whenever a task, queue,
+//     timer or semaphore is created.  It is also called by various parts of the
+//     demo application.  If heap_1.c or heap_2.c are used, then the size of the
+//     heap available to pvPortMalloc() is defined by configTOTAL_HEAP_SIZE in
+//     FreeRTOSConfig.h, and the xPortGetFreeHeapSize() API function can be used
+//     to query the size of free heap space that remains (although it does not
+//     provide information on how the remaining heap might be fragmented). */
+//     taskDISABLE_INTERRUPTS();
+//     for (;;)
+//         ;
+// }
 /*-----------------------------------------------------------*/
 
-void vApplicationIdleHook(void)
-{
-    /* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set
-    to 1 in FreeRTOSConfig.h.  It will be called on each iteration of the idle
-    task.  It is essential that code added to this hook function never attempts
-    to block in any way (for example, call xQueueReceive() with a block time
-    specified, or call vTaskDelay()).  If the application makes use of the
-    vTaskDelete() API function (as this demo application does) then it is also
-    important that vApplicationIdleHook() is permitted to return to its calling
-    function, because it is the responsibility of the idle task to clean up
-    memory allocated by the kernel to any task that has since been deleted. */
-}
+// void vApplicationIdleHook(void)
+// {
+//     /* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is
+//     set to 1 in FreeRTOSConfig.h.  It will be called on each iteration of the
+//     idle task.  It is essential that code added to this hook function never
+//     attempts to block in any way (for example, call xQueueReceive() with a
+//     block time specified, or call vTaskDelay()).  If the application makes
+//     use of the vTaskDelete() API function (as this demo application does)
+//     then it is also important that vApplicationIdleHook() is permitted to
+//     return to its calling function, because it is the responsibility of the
+//     idle task to clean up memory allocated by the kernel to any task that has
+//     since been deleted. */
+// }
 /*-----------------------------------------------------------*/
 
-void vApplicationStackOverflowHook(xTaskHandle pxTask, signed char *pcTaskName)
-{
-    (void) pcTaskName;
-    (void) pxTask;
+// void vApplicationStackOverflowHook(xTaskHandle pxTask, signed char
+// *pcTaskName)
+// {
+//     (void) pcTaskName;
+//     (void) pxTask;
 
-    /* Run time stack overflow checking is performed if
-    configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
-    function is called if a stack overflow is detected. */
-    taskDISABLE_INTERRUPTS();
-    for (;;)
-        ;
-}
+//     /* Run time stack overflow checking is performed if
+//     configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
+//     function is called if a stack overflow is detected. */
+//     taskDISABLE_INTERRUPTS();
+//     for (;;)
+//         ;
+// }
 /*-----------------------------------------------------------*/
 
-void vApplicationTickHook(void)
-{
-    /* This function will be called by each tick interrupt if
-    configUSE_TICK_HOOK is set to 1 in FreeRTOSConfig.h.  User code can be
-    added here, but the tick hook is called from an interrupt context, so
-    code must not attempt to block, and only the interrupt safe FreeRTOS API
-    functions can be used (those that end in FromISR()).  */
+// void vApplicationTickHook(void)
+// {
+//     /* This function will be called by each tick interrupt if
+//     configUSE_TICK_HOOK is set to 1 in FreeRTOSConfig.h.  User code can be
+//     added here, but the tick hook is called from an interrupt context, so
+//     code must not attempt to block, and only the interrupt safe FreeRTOS API
+//     functions can be used (those that end in FromISR()).  */
 
-#if (mainCREATE_SIMPLE_BLINKY_DEMO_ONLY == 0)
-    {
-        /* In this case the tick hook is used as part of the queue set test. */
-        // FIXME
-        // vQueueSetAccessQueueSetFromISR();
-    }
-#endif /* mainCREATE_SIMPLE_BLINKY_DEMO_ONLY */
-}
+// #if (mainCREATE_SIMPLE_BLINKY_DEMO_ONLY == 0)
+//     {
+//         /* In this case the tick hook is used as part of the queue set test.
+//         */
+//         // FIXME
+//         // vQueueSetAccessQueueSetFromISR();
+//     }
+// #endif /* mainCREATE_SIMPLE_BLINKY_DEMO_ONLY */
+// }
